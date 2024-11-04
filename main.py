743184fa8d0d19639ede7b40e072886f9b89f018
Zavaon Taylor <taylozav000@warren.k12.in.us>
2024-11-04 09:53:03 -0500

    tuple

---
+++ main.py
@@ -0,0 +1,20 @@
+#SECTION 1
+fruit_tuple = ("apple", "banana", "cherry", "date")
+print(fruit_tuple[0])
+print(fruit_tuple[3])
+slice_tuple = fruit_tuple[1 : 3]
+print(slice_tuple)
+
+#SECTION 2
+#TODO: create num_tuple as per instruction
+num_tuple = (3, 5, 3, 2, 8, 3, 7)
+#TODO: look at the count method in the notes
+count:(num_tuple[3])
+#TODO: Look at the index method in the notes
+index = (num_tuple[0])
+#SECTION 3
+#TODO: creat person_info tuple
+person_info = ("Alice", 28, "Engineer")
+#TODO: Look at the notes for unpacking
+person_info = ("name", "age", "profession")
+#TODO: print the variables.
